719cc06e98a3e9bc460ac68434526284b552024f
Italo Bruno <italobrunosantos@live.com>
2018-06-08 17:55:46 -0300

    Criando operacao de instanciacao

--- src/main/java/com/udemy/cursomc/CursomcApplication.java
+++ src/main/java/com/udemy/cursomc/CursomcApplication.java
@@ -1,12 +1,35 @@
 package com.udemy.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.udemy.cursomc.domain.Categoria;
+import com.udemy.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository repository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	
+	//Instaciar obejtos automaticamente ao iniciar 
+	@Override
+	public void run(String... args) throws Exception {
+	Categoria cat1 = new Categoria(null, "Informatica");
+	Categoria cat2 = new Categoria(null, "Escritorio");
+	
+	//Salvar no Banco de Dados
+	repository.save(Arrays.asList(cat1,cat2));
+	
+		
 	}
 }
