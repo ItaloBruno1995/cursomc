94e6cd99d02d0dfe8e99fcae8d9105e645d2bfe8
Italo Bruno <italobrunosantos@live.com>
2018-06-09 18:39:41 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/udemy/cursomc/CursomcApplication.java
+++ src/main/java/com/udemy/cursomc/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.udemy.cursomc.domain.Categoria;
+import com.udemy.cursomc.domain.Produto;
 import com.udemy.cursomc.repositories.CategoriaRepository;
+import com.udemy.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository repository;
+	
+	@Autowired
+	private ProdutoRepository repositoyproduto;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -27,9 +32,26 @@ public class CursomcApplication implements CommandLineRunner {
 	Categoria cat1 = new Categoria(null, "Informatica");
 	Categoria cat2 = new Categoria(null, "Escritorio");
 	
-	//Salvar no Banco de Dados
-	repository.save(Arrays.asList(cat1,cat2));
+	Produto p1 = new Produto(null, "Computador", 2000.00);
 	
+	Produto p2 = new Produto(null, "Impressora", 800.00);
+	
+	Produto p3 = new Produto(null, "Mouse", 80.000);
+	
+	//ASSOCIAR CARETOGIA AO PRODUTO:
+	cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+	cat2.getProdutos().addAll(Arrays.asList(p2));
+	
+	//ASSOCIAR PRODUTO COM SUA CATEGORIA
+	p1.getCategorias().addAll(Arrays.asList(cat1));
+	p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+	p3.getCategorias().addAll(Arrays.asList(cat1));
+	
+	
+	
+	//SALVAR NO BANCO DE DADOS
+	repository.save(Arrays.asList(cat1,cat2));
+	repositoyproduto.save(Arrays.asList(p1,p2,p3));
 		
 	}
 }
---
+++ src/main/java/com/udemy/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,14 @@
+package com.udemy.cursomc.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.udemy.cursomc.domain.Produto;
+
+@Repository                                              //CLASS     //TIPO ID
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+	
+	
+}
